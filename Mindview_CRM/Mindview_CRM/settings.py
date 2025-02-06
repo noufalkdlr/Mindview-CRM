d7ee630cf24ddb199439ecde8029a7a682cc6da4
noufalkakdlr <noufalkakdlr@gmail.com>
2025-02-06 18:24:40 +0530

    model

--- Mindview_CRM/Mindview_CRM/settings.py
+++ Mindview_CRM/Mindview_CRM/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Mindview_CRM.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Mindview_CRM/employees/models.py
+++ Mindview_CRM/employees/models.py
@@ -1,3 +1,18 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 # Create your models here.
+aadhaar_validator = RegexValidator(
+     r'^\d{12}$',  # Ensures exactly 12 digits
+    'Aadhaar number must be exactly 12 digits.'   
+)
+
+class EmployeesDetails(models.Model):
+    image = models.ImageField(upload_to='employee_image')
+    name = models.CharField(max_length=100)
+    designation = models.CharField()
+    d_o_b = models.DateField()
+    date_of_joining = models.DateField()
+    blood_group = models.CharField(max_length=3)
+    Aadhaar_no = models.CharField(max_length=12, unique=True,validators=[aadhaar_validator],)
+    
