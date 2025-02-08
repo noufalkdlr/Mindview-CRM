935987e90ed63ef7ed86bfaddb7eb5c5d3d459ec
noufalkakdlr <noufalkakdlr@gmail.com>
2025-02-08 08:28:49 +0530

    change database

--- Mindview_CRM/Mindview_CRM/settings.py
+++ Mindview_CRM/Mindview_CRM/settings.py
@@ -82,6 +82,17 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mindview_db', 
+        'USER': 'root', 
+        'PASSWORD': 'password123',  
+        'HOST': 'localhost', 
+        'PORT': '3306', 
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
